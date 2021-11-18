0224de2774cec4f369ac7b60fc5155e201dbd4e8
Rui Cerqueira <a100537@alunos.uminho.pt>
2021-11-18 10:00:32 +0000

    adicionei testas na Tarefa1 e iniciei a Tarefa 2

--- src/Tarefa2_2021li1g099.hs
+++ src/Tarefa2_2021li1g099.hs
@@ -10,8 +10,18 @@ module Tarefa2_2021li1g099 where
 
 import LI12122
 
+{-
 constroiMapa :: [(Peca, Coordenadas)] -> Mapa
-constroiMapa pecas = undefined
-
+constroiMapa [] = []
+constroimapa (Peca, (x,y)) =
+-}
 desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
 desconstroiMapa mapa = undefined
+
+-- Funçao que filtra as coordenadas com igual y e as ordena
+ordenaY :: [(Peca, Coordenadas)] -> [Peca, Coordenadas]
+ordenaY a@(Peca, (x1,y1)) ((Peca, (x2,y2)):t)
+
+-- Função que ordene as listas obtidas na função ordenaY
+ordenaPecas :: [(Peca, Coordenadas)] -> [Peca, Coordenadas]
+ordenaPecas a@(Peca, (x1,y1)) ((Peca, (x2,y2)):t)
--- tests/Tarefa1_2021li1g099_Spec.hs
+++ tests/Tarefa1_2021li1g099_Spec.hs
@@ -11,4 +11,8 @@ testsT1 =
     [ "Tarefa 1 - Teste Valida Mapa m1r" ~: validaPotencialMapa m1 ~=? True
     , "Tarefa 1 - Teste Valida Mapa vazio" ~: validaPotencialMapa [] ~=? False
     , "Tarefa 1 - Teste Valida Mapa com 2 portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] ~=?  False
+    , "Tarefa 1 - Teste Valida Mapa com uma caixa em cima de uma porta" ~: validaPotencialMapa [(Porta, (1,1)), (Caixa, (1,2))] ~=? False
+    , "Tarefa 1 - Teste Valida Mapa sem vazios" ~: validaVazios [(Bloco, (0,0)), (Bloco, (1,0)), (Bloco, (1,0)), (Caixa, (1,1))] ~=? False
+    , "Tarefa 1 - Teste Valida Mapa sem chao" ~: validaPotencialMapa [(Bloco, (0,0)), (Bloco, (0,2))] ~=? False
+    , "Tarefa 1 - Teste Valida Mapa com chao como teto do mapa" ~: validaPotencialMapa [(Bloco, (0,0)), (Bloco, (1,0)), (Bloco, (2,0))] ~=? False
     ]
