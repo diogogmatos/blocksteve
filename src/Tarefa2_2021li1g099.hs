6e11cdbd2e94a5fc529d873b16272e556ec0d97b
Rui Cerqueira <a100537@alunos.uminho.pt>
2021-11-18 11:31:11 +0000

    cont. Tarefa2

--- src/Tarefa2_2021li1g099.hs
+++ src/Tarefa2_2021li1g099.hs
@@ -19,9 +19,14 @@ desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
 desconstroiMapa mapa = undefined
 
 -- Funçao que filtra as coordenadas com igual y e as ordena
-ordenaY :: [(Peca, Coordenadas)] -> [Peca, Coordenadas]
-ordenaY a@(Peca, (x1,y1)) ((Peca, (x2,y2)):t)
+--filtraY:: [(Peca, Coordenadas)] -> [Peca, Coordenadas]
+--filtraY (p, (x1,y1)) ((p, (x2,y2):t)
 
--- Função que ordene as listas obtidas na função ordenaY
-ordenaPecas :: [(Peca, Coordenadas)] -> [Peca, Coordenadas]
-ordenaPecas a@(Peca, (x1,y1)) ((Peca, (x2,y2)):t)
+-- Função que ordene as listas em relaçao ao x
+ordenaX :: [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]
+ordenaX [] = []
+ordenaX ((p, (x,y)):t) = ordenaX (menor t) ++ [(p, (x,y))] ++ ordenaX (maior t)
+    where maior [] = []
+          maior ((p, (xs,ys)):l) = if xs >= x then (p, (xs,ys)) : maior l else maior l
+          menor [] = []
+          menor ((p, (xs,ys)):l) = if xs < x then (p, (xs,ys)) : menor l else menor l
