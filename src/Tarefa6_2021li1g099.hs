e78187f668df9f08426e368c444341f67cefa38f
a100741 <a100741@alunos.uminho.pt>
2021-12-29 18:50:33 +0000

    T6 - Resolução para mapas sem caixas

--- src/Tarefa6_2021li1g099.hs
+++ src/Tarefa6_2021li1g099.hs
@@ -1,35 +1,59 @@
 {- |
-Module      : Tarefa6_2021li1gXXX
+Module      : Tarefa6_2021li1g099
 Description : Resolução de um puzzle
 
 Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
 -}
-module Tarefa6_2021li1gXXX where
+
+module Tarefa6_2021li1g099 where
 
 import LI12122
 import Tarefa1_2021li1g099
+import Tarefa2_2021li1g099
 import Tarefa4_2021li1g099
 
 resolveJogo :: Int -> Jogo -> Maybe [Movimento]
-resolveJogo i jogo = undefined
+resolveJogo i j@(Jogo m jogador) | not (validaPotencialMapa (desconstroiMapa m)) = Nothing
+                                 | filter aux (desconstroiMapa m) == [] = Just (resolveSemCaixas i (desconstroiMapa m) (desconstroiMapa m) jogador)
+                                 | otherwise = Nothing
+                                 where aux (p,(_,_)) = p == Caixa
 
+--
 
---conta o numero de jogadas de todos os movimentos executados
-contaJogadas :: Int -> [Movimento] -> Int
-contaJogadas acc [] = acc
-contaJogadas acc (x:xs) | x == Trepar = contaJogadas (acc+1) xs
-                        | x == AndarEsquerda = contaJogadas (acc+1) xs
-                        | x == AndarDireita = contaJogadas (acc+1) xs
-                        | x == InterageCaixa = contaJogadas (acc+1) xs
-                        | otherwise = contaJogadas acc xs
+resolveSemCaixas :: Int -> [(Peca,Coordenadas)] -> [(Peca,Coordenadas)] -> Jogador -> [Movimento]
+resolveSemCaixas _ [] _ _ = []
+resolveSemCaixas i (m:ms) mapa j@(Jogador (x,y) d b)
+    | p1 == x && p2 == y = []
+    | ((p1 - x) > 0) && b = 
+        if (notElem (Bloco,(x+1,y)) mapa || notElem (Bloco,(x+1,y-1)) mapa)
+        then AndarDireita : (resolveSemCaixas i ms mapa (Jogador (x+1,y) d b))
+        else if d /= Este
+        then [AndarDireita,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
+        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
+    | ((p1 - x) < 0) && b = 
+        if (notElem (Bloco,(x-1,y)) mapa || notElem (Bloco,(x-1,y-1)) mapa)
+        then AndarEsquerda : (resolveSemCaixas i ms mapa (Jogador (x-1,y) d b))
+        else if d /= Oeste
+        then [AndarEsquerda,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
+        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
+    | (p1 - x) > 0 = 
+        if (notElem (Bloco,(x+1,y)) mapa)
+        then AndarDireita : (resolveSemCaixas i ms mapa (Jogador (x+1,y) d b))
+        else if d /= Este
+        then [AndarDireita,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
+        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
+    | (p1 - x) < 0 =
+        if (notElem (Bloco,(x-1,y)) mapa)
+        then AndarEsquerda : (resolveSemCaixas i ms mapa (Jogador (x-1,y) d b))
+        else if d /= Oeste
+        then [AndarEsquerda,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
+        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
+    | otherwise = []
+    where (p1,p2) = givePorta (filter aux mapa)
+          aux (p,(_,_)) = p == Porta
 
--- conta o numero de jogadas de um certo movimento                        
-{-                   
-contaJogadas :: Movimento -> Int -> [Movimento] -> Int
-contaJogadas mov acc [] = acc
-contaJogadas mov acc (x:xs) | x == mov = contaJogadas mov (acc+1) xs
-                            | otherwise = contaJogadas mov acc xs
--}
+givePorta :: [(Peca,Coordenadas)] -> Coordenadas
+givePorta ((_,(a,b)):t) = (a,b)
 
-numMaxJogadas :: Int -> [Movimento] -> Bool
-numMaxJogadas x l = x >= (contaJogadas 0 l)
+--
+
