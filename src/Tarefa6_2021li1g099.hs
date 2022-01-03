c4213deedc6f463e6dc3354a73f02ea90fd65504
a100741 <a100741@alunos.uminho.pt>
2022-01-03 16:59:07 +0000

    T6 - Melhoramento de Sintaxe em 'resolveSemCaixas'

--- src/Tarefa6_2021li1g099.hs
+++ src/Tarefa6_2021li1g099.hs
@@ -14,7 +14,7 @@ import Tarefa4_2021li1g099
 
 resolveJogo :: Int -> Jogo -> Maybe [Movimento]
 resolveJogo i j@(Jogo m jogador) | not (validaPotencialMapa (desconstroiMapa m)) = Nothing
-                                 | filter aux (desconstroiMapa m) == [] = Just (resolveSemCaixas i (desconstroiMapa m) (desconstroiMapa m) jogador)
+                                 | null (filter aux (desconstroiMapa m)) = Just (resolveSemCaixas i (desconstroiMapa m) (desconstroiMapa m) jogador)
                                  | otherwise = Nothing
                                  where aux (p,(_,_)) = p == Caixa
 
@@ -24,30 +24,30 @@ resolveSemCaixas :: Int -> [(Peca,Coordenadas)] -> [(Peca,Coordenadas)] -> Jogad
 resolveSemCaixas _ [] _ _ = []
 resolveSemCaixas i (m:ms) mapa j@(Jogador (x,y) d b)
     | p1 == x && p2 == y = []
-    | ((p1 - x) > 0) && b = 
-        if (notElem (Bloco,(x+1,y)) mapa || notElem (Bloco,(x+1,y-1)) mapa)
-        then AndarDireita : (resolveSemCaixas i ms mapa (Jogador (x+1,y) d b))
+    | ((p1 - x) > 0) && b =
+        if notElem (Bloco,(x+1,y)) mapa || notElem (Bloco,(x+1,y-1)) mapa
+        then AndarDireita : resolveSemCaixas i ms mapa (Jogador (x+1,y) d b)
         else if d /= Este
-        then [AndarDireita,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
-        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
-    | ((p1 - x) < 0) && b = 
-        if (notElem (Bloco,(x-1,y)) mapa || notElem (Bloco,(x-1,y-1)) mapa)
-        then AndarEsquerda : (resolveSemCaixas i ms mapa (Jogador (x-1,y) d b))
+        then [AndarDireita,Trepar] ++ resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b)
+        else Trepar : resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b)
+    | ((p1 - x) < 0) && b =
+        if notElem (Bloco,(x-1,y)) mapa || notElem (Bloco,(x-1,y-1)) mapa
+        then AndarEsquerda : resolveSemCaixas i ms mapa (Jogador (x-1,y) d b)
         else if d /= Oeste
-        then [AndarEsquerda,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
-        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
-    | (p1 - x) > 0 = 
-        if (notElem (Bloco,(x+1,y)) mapa)
-        then AndarDireita : (resolveSemCaixas i ms mapa (Jogador (x+1,y) d b))
+        then [AndarEsquerda,Trepar] ++ resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b)
+        else Trepar : resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b)
+    | (p1 - x) > 0 =
+        if notElem (Bloco,(x+1,y)) mapa
+        then AndarDireita : resolveSemCaixas i ms mapa (Jogador (x+1,y) d b)
         else if d /= Este
-        then [AndarDireita,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
-        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b))
+        then [AndarDireita,Trepar] ++ resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b)
+        else Trepar : resolveSemCaixas i ms mapa (Jogador (x+1,y-1) d b)
     | (p1 - x) < 0 =
         if (notElem (Bloco,(x-1,y)) mapa)
-        then AndarEsquerda : (resolveSemCaixas i ms mapa (Jogador (x-1,y) d b))
+        then AndarEsquerda : resolveSemCaixas i ms mapa (Jogador (x-1,y) d b)
         else if d /= Oeste
-        then [AndarEsquerda,Trepar] ++ (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
-        else Trepar : (resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b))
+        then [AndarEsquerda,Trepar] ++ resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b)
+        else Trepar : resolveSemCaixas i ms mapa (Jogador (x-1,y-1) d b)
     | otherwise = []
     where (p1,p2) = givePorta (filter aux mapa)
           aux (p,(_,_)) = p == Porta
